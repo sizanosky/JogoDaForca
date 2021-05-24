72681fbc7ad4bc929980e90993f41c2675fc9004
Marcos Fabricio Sizanosky <fabricio_sizanosky@hotmail.com>
2021-05-24 03:48:47 -0300

    Criadas as funções gerar_palavra_secreta, verificar_palavra_informada e total_tentativas

--- helpers.py
+++ helpers.py
@@ -12,3 +12,39 @@ def gerar_palavra_secreta():
         palavra = f_obj.read().splitlines()
 
     return random.choice(palavra)
+
+
+def verificar_palavra_informada(palavra_secreta, suas_tentativas, tentativa):
+    """
+    - Função verifica se a letra informada está correta.
+    :param palavra_secreta: Gerada com base no arquivo dados.txt.
+    :param suas_tentativas: Lista com todas as tentativas.
+    :param tentativa: Letra inserida na jogada atual.
+    :return: Retorna um status.
+    """
+    status = ''  # O status precisa ser zerado sempre que a função for chamada.
+    acertos = 0  # Também precisa ser zerado a cada jogada/tentativa.
+
+    for letra in palavra_secreta:
+        if letra.lower() in suas_tentativas:
+            status += letra
+        else:
+            status += '*'
+
+        if letra.lower() == tentativa.lower():
+            acertos += 1
+
+    print(f"\n- Acertou {acertos} letras '{tentativa}'")
+
+    return status
+
+
+def total_tentativas(palavra_secreta):
+    """
+    - Define o número de tentativas de acordo com o tamanho da palavra secreta.
+    :param palavra_secreta: Gerada randomicamente.
+    :return: Quantidade de tentativas.
+    """
+    chances = len(palavra_secreta)
+
+    return chances + TENTATIVAS_ADICIONAIS
